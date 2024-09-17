644c2f2c63acd4e2c889106799200be4faf2ed6b
andsu <andsu.marks.r@gmail.com>
2024-09-17 10:26:08 -0300

    created migration to add coursesId field on the courses_tags table

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -3,9 +3,10 @@ import { dataSourceOptions } from "./database.module";
 import { CreateCourseTable1726519487687 } from "src/migrations/1726519487687-CreateCourseTable";
 import { CreateTagsTable1726576341647 } from "src/migrations/1726576341647-CreateTagsTable";
 import { CreateCoursesTagsTable1726577692562 } from "src/migrations/1726577692562-CreateCoursesTagsTable";
+import { AddCoursesIdToCoursesTagsTable1726578371421 } from "src/migrations/1726578371421-AddCoursesIdToCoursesTagsTable";
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCourseTable1726519487687, CreateTagsTable1726576341647, CreateCoursesTagsTable1726577692562]
+  migrations: [CreateCourseTable1726519487687, CreateTagsTable1726576341647, CreateCoursesTagsTable1726577692562, AddCoursesIdToCoursesTagsTable1726578371421]
 })
