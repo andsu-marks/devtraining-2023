610016483592eb1d8c8c03567feb5fa0e2ec8e0f
andsu <andsu.marks.r@gmail.com>
2024-09-20 17:48:11 -0300

    added @nestjs/config and configured

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,10 +1,23 @@
-import { DataSource } from "typeorm";
-import { dataSourceOptions } from "./database.module";
+import 'dotenv/config';
+import { DataSource, DataSourceOptions } from "typeorm";
 import { CreateCourseTable1726519487687 } from "src/migrations/1726519487687-CreateCourseTable";
 import { CreateTagsTable1726576341647 } from "src/migrations/1726576341647-CreateTagsTable";
 import { CreateCoursesTagsTable1726577692562 } from "src/migrations/1726577692562-CreateCoursesTagsTable";
 import { AddCoursesIdToCoursesTagsTable1726578371421 } from "src/migrations/1726578371421-AddCoursesIdToCoursesTagsTable";
 import { AddTagsIdToCoursesTagsTable1726579639006 } from "src/migrations/1726579639006-AddTagsIdToCoursesTagsTable";
+import { Course } from "src/courses/entities/courses.entity";
+import { Tag } from "src/courses/entities/tags.entity";
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [Course, Tag],
+  synchronize: true
+}
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
